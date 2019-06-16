0c929b4585a1adb04c1b0518ec19dad370c82786
sonal satpute <sonal.satpute@gmail.com>
2019-06-16 13:16:07 +0530

    refactored Title case transformer tests

--- src/test/java/com/sonalsatpute/elt/extractor/tests/ContentExtractorTests.java
+++ src/test/java/com/sonalsatpute/elt/extractor/tests/ContentExtractorTests.java
@@ -1,4 +1,4 @@
-package com.sonalsatpute.elt.test.extractor;
+package com.sonalsatpute.elt.extractor.tests;
 
 import com.sonalsatpute.etl.extractor.IContentExtractor;
 import com.sonalsatpute.etl.extractor.StringContentExtractor;
@@ -6,7 +6,6 @@ import com.sonalsatpute.etl.extractor.TextFileContentExtractor;
 import org.junit.Test;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,12 +24,13 @@ public class ContentExtractorTests {
 
     @Test
     public void Text_file_content_extractor_should_convert_string_to_input_stream() throws IOException {
+        String fileId = "src/main/resources/input-file.txt";
         IContentExtractor reader = new TextFileContentExtractor();
 
-        InputStream actualInputStream = reader.contentAsStream("src/main/resources/input-file.txt");
+        InputStream actualInputStream = reader.contentAsStream(fileId);
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(actualInputStream));
-        String fileContext = "I  am a    great coder who loves to solve  real world problems.";
-        assertEquals(fileContext, bufferedReader.readLine());
+        String expectedFileContent = "I  am a    great coder who loves to solve  real world problems.";
+        assertEquals(expectedFileContent, bufferedReader.readLine());
     }
 }
---
+++ src/test/java/com/sonalsatpute/elt/transformer/tests/TitleCaseTransformerTests.java
@@ -0,0 +1,35 @@
+package com.sonalsatpute.elt.transformer.tests;
+
+import com.sonalsatpute.etl.transformer.ICaseTransformer;
+import com.sonalsatpute.etl.transformer.TitleCaseTransformer;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class TitleCaseTransformerTests {
+
+    @Test
+    public void Title_case_transformer_should_convert_first_letter_of_all_the_words_to_capital_letter() {
+        String content = "I am a great coder who loves to solve real world problems.";
+        ICaseTransformer transformer = new TitleCaseTransformer();
+
+        String actual = transformer.process(content);
+
+        String expected = "I Am A Great Coder Who Loves To Solve Real World Problems.";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Title_case_transformer_should_replace_multiple_sequential_space_with_single_space() {
+        String content = "I    am a great     coder who loves    to solve   real world problems.";
+        ICaseTransformer transformer = new TitleCaseTransformer();
+
+        String actual = transformer.process(content);
+
+        String expected = "I Am A Great Coder Who Loves To Solve Real World Problems.";
+        assertEquals(expected, actual);
+    }
+}
+
