d69c47d7d6e4b0f79200175bf3276cf9834dfb6f
sonal satpute <sonal.satpute@gmail.com>
2019-06-17 20:28:54 +0530

    the app is ready with ETL

--- src/main/java/com/sonalsatpute/etl/app.java
+++ src/main/java/com/sonalsatpute/etl/app.java
@@ -1,7 +1,25 @@
 package com.sonalsatpute.etl;
 
+import com.sonalsatpute.etl.extractor.FileContentExtractor;
+import com.sonalsatpute.etl.loader.FileContentLoader;
+import com.sonalsatpute.etl.transformer.TitleCaseTransformer;
+
+import java.io.IOException;
+
 public class app {
     public static void main(String[] args) {
+        String inputFileId = "src/main/resources/input-file.txt";
+        String outputFileId = "src/main/resources/output-file.txt";
 
+        TextContentProcess process = new TextContentProcess(
+                new FileContentExtractor(inputFileId),
+                new TitleCaseTransformer(),
+                new FileContentLoader(outputFileId));
+
+        try {
+            process.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
